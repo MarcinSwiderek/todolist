d6538ffd319ec076d4153eb825097424cb5b4865
Marcin Swiderek <svdr23@gmail.com>
2015-03-13 14:25:18 +0100

    Dodanie klasy task

---
+++ task.php
@@ -0,0 +1,73 @@
+<?php
+
+class Task{
+	private $title="";
+	private $text="";
+	
+	public function __construct($newTitle,$newText) {
+		$this->setText($newText);
+		$this->setTitle($newTitle);
+	}
+	
+	public function setTitle($newTitle){
+		if(is_string($newTitle)) {
+			$this->title=$newTitle;
+		}
+	}
+	public function getTitle() {
+		return $this->title;
+	}
+	
+	public function setText($newText){
+		if(is_string($newText)) {
+			$this->text=$newText;
+		}
+	}
+	public function getText() {
+		return $this->text;
+	}
+	
+	public function intoHTML() {
+		$ret="";
+		$ret .= "Tytul: ".$this->getTitle()."<br>";
+		$ret .= "Tresc: ".$this->getText()."<br>";
+		
+		return $ret;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
